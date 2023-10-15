b8979a270df6e18974d0f00bc9683429cf342f1e
IvanyutinSA <ivanyutin2004@gmail.com>
2023-10-15 12:49:52 +0600

    implemented basics

--- Assets/Scripts/Player/CameraFollow.cs
+++ Assets/Scripts/Player/CameraFollow.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Camera : MonoBehaviour
 {
+    public Transform target;
+    public Vector3 offset;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,6 @@ public class Camera : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position = target.position + offset;
     }
 }
