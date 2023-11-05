4b5c73cd853a42a8dfec689aca6a8fbb0abef0c9
Dmitry505 <79017954+Dmitry505@users.noreply.github.com>
2023-11-05 18:48:45 +0600

    Добавил движение врага, чуть позже подключу взаимодействие

---
+++ Assets/Scripts/Enemy/Enemy_Passive.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static UnityEngine.RuleTile.TilingRuleOutput;
+
+public class Enemy_Passive : MonoBehaviour
+{
+
+    public float speed = 3; // Скорость движения объекта
+    public float position_start = 3.4f ; // каординаты от котроыой и до которой движиться объект
+    public float position_fnish = 18.3f ;
+
+    private bool movingForward = true; // Флаг, указывающий, двигается ли объект вперед
+
+    private void Update()
+    {
+        move();
+//        passive_attack();
+    }
+
+    void move() 
+    {
+        if (movingForward)
+        {
+            if (transform.position.x < position_fnish)
+            {
+                transform.Translate(transform.right * speed * Time.deltaTime); // Движение вперед
+
+            }
+            else
+            {
+                movingForward = false;
+            }
+        }
+        else
+        {
+            if (transform.position.x > position_start)
+            {
+                transform.Translate(-transform.right * speed * Time.deltaTime); // Движение back
+
+            }
+            else
+            {
+                movingForward = true;
+            }
+        }
+    }
+
+/*    void passive_attack() 
+    {
+        
+    }*/
+}
