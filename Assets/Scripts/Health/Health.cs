a974197c20644ddc79f9af436382cc8ed3a5cc26
Nikolay Koren <nikolai.koren2004@gmail.com>
2023-12-20 00:32:00 +0600

    playerDie

--- Assets/Scripts/Health/Health.cs
+++ Assets/Scripts/Health/Health.cs
@@ -1,37 +1,14 @@
-/*using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Health : MonoBehaviour
-{
-    [SerializeField] private int lives = 5;
-    [SerializeField] private int livesMax = 5;
-
-    public void Change(int amount)
-    {
-        lives += amount;
-        if (lives > livesMax)
-        {
-            lives = livesMax;
-        }
-        Debug.Log(lives);
-        if (lives <= 0)
-        {
-            Debug.Log("Object destroyed");
-        }
-    }
-    public int GetCurrentLives() { return lives; }
-    public int GetMaxLives() { return livesMax; }
-}*/
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class Health : MonoBehaviour
 {
     [SerializeField] private int lives = 5;
     [SerializeField] private int livesMax = 5;
+    [SerializeField] private bool isLive = true;
 
     private void Awake()
     {
@@ -47,10 +24,20 @@ public class Health : MonoBehaviour
         Debug.Log(lives);
         if (lives <= 0)
         {
-            Debug.Log("Object destroyed");
+            isLive = false;
+            Invoke("PlayerDie", 2);
+            //PlayerDie();
         }
     }
+
+    private void PlayerDie()
+    {
+        Debug.Log("Object destroyed");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
     public int GetCurrentLives() { return lives; }
     public int GetMaxLives() { return livesMax; }
+    public bool IsLive { get {  return isLive; } }
 }
 
